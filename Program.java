c1e2e37f81ef8b73c17209d008fa52e41fc6b7db
HasiniMorasa <hmorasa9@gmail.com>
2024-09-06 09:28:00 -0700

    Working on Program 8

--- Program.java
+++ Program.java
@@ -1,7 +1,30 @@
-//import stuff here
-
+import java.utilScanner;
 //Your code here
-
+public class Program8{ 
+     
+    public static void main(String[] args) { 
+     Scanner scanner = new Scanner(System.in); 
+        
+    //Declare veriages 
+    int a; 
+    int b;
+    double average;
+    
+    //Reading the variables 
+    System.out.println("Enter the first 
+    
+    
+    
+    // Calculate the numbers
+    int sum = a + b;
+    int difference = a -b;
+    int product = a * b;
+    double average = a + b/2;
+    int distance = [a-b]; 
+    
+    
+    
+}
 //Paste console output below:
 /*
 
