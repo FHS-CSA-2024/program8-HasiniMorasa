5b57d30fe26cef28a3b7c986ca1193b336504239
HasiniMorasa <hmorasa9@gmail.com>
2024-09-06 19:27:41 -0700

    Finished Program8

--- Program.java
+++ Program.java
@@ -1,4 +1,6 @@
-import java.utilScanner;
+import java.util.Scanner;
+
+
 //Your code here
 public class Program8{ 
      
@@ -6,24 +8,56 @@ public class Program8{
      Scanner scanner = new Scanner(System.in); 
         
     //Declare veriages 
-    int a; 
-    int b;
-    double average;
+    
     
     //Reading the variables 
-    System.out.println("Enter the first 
+    System.out.println("Enter number 1: ");
+    int number1 = scanner.nextInt();
+    System.out.println("Enter number 2: ");
+    int number2 = scanner.nextInt();
     
+     System.out.println("Original numbers are " + number1 + " and " + number2);
     
     
     // Calculate the numbers
-    int sum = a + b;
-    int difference = a -b;
-    int product = a * b;
-    double average = a + b/2;
-    int distance = [a-b]; 
+    int sum = number1 + number2;
+    System.out.println("Sum = " + sum);
     
+    int difference = number1 - number2 ;
     
+    System.out.println("Difference = " + difference);
     
+    int product = number1  * number2;
+    
+    System.out.println("Product = " + product);
+    
+    double average = (number1 + number2)/2.0;
+    
+    System.out.println("Average = " + average);
+    
+    int absolute = Math.abs(difference);
+    
+    System.out.println("Absolute value = " + absolute);
+    
+    int max = 0;
+    int min = 0;
+    
+    if(number1 > number2)
+    {
+        max = number1;
+        min = number2;
+    }
+    else
+    {
+        max = number2;
+        min = number1;
+    }
+    
+    System.out.println("Maximum = " + max);
+    
+    System.out.println("Minimum = " + min);
+    
+  }
 }
 //Paste console output below:
 /*
